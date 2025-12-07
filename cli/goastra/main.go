bafd30d46bd37fd8196d909d81c905fccf705146
Channdev <xenn1337j@gmail.com>
2025-12-08 02:18:41 +0800

    Move main.go to goastra folder for correct binary name

---
+++ cli/goastra/main.go
@@ -0,0 +1,19 @@
+/*
+ * GoAstra CLI - Main Entry Point
+ *
+ * This file serves as the bootstrap for the GoAstra command-line interface.
+ * It initializes the root command and delegates execution to the cmd package.
+ */
+package main
+
+import (
+	"os"
+
+	"github.com/channdev/goastra/cli/cmd"
+)
+
+func main() {
+	if err := cmd.Execute(); err != nil {
+		os.Exit(1)
+	}
+}
